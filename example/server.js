a649aa3bd1220f4f4881b505f2097b7505608640
macleo <52macleo@gmail.com>
2019-07-06 01:03:08 +0800

    modify server.js

--- example/server.js
+++ example/server.js
@@ -1,8 +1,9 @@
 var connect = require('connect');
-
+var serveStatic = require('serve-static');
+var logger = require('morgan');
 connect()
-  .use(connect.logger('dev'))
-  .use(connect.static(__dirname))
-  .use(connect.static(__dirname + '/../lib/'))
+  .use(logger('dev'))
+  .use(serveStatic(__dirname))
+  .use(serveStatic(__dirname + '/../lib/'))
   .listen(8080);
 console.log('Server running at http://0.0.0.0:8080/');
